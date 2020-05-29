5a780ca24ffdbc73259d4ff79d69d07f6cee0653
Christie Molloy <cmolloy@redhat.com>
2020-05-29 17:19:13 -0400

    add all the files

---
+++ src/app/Dashboard/Dashboard.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { PageSection, Title } from '@patternfly/react-core';
+
+const Dashboard: React.FunctionComponent<{}> = () => (
+  <PageSection>
+    <Title headingLevel="h1" size="lg">Hi there! Thanks for participating in this workshop! Add your code to this page!</Title>
+  </PageSection>
+)
+
+export { Dashboard };
